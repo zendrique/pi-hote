5f11f7aaa65d82af5242196d843a12006bb229ee
zendrique <zendrique19@gmail.com>
2021-10-02 17:51:16 +0200

    Optimisation de la bande passante

--- pi-hot.sh
+++ pi-hot.sh
@@ -4,19 +4,25 @@ warn_temp="60"
 webhooks_url=""
 shutdowncmd="poweroff"
 temp="$(vcgencmd measure_temp | egrep -o '[0-9]*\.[0-9]*')"
-ip="$(curl -s https://api.ipify.org)"
 arg=$1
 
 # Programme
 if [ "$1" = "--manual" ] ; then
+    getip
     curl -H "Content-Type: application/json" -d '{"username": "Raspberry Pi", "content": "'"Demande de vérification manuelle \nMa température est de : $temp °C."'"}' $webhooks_url
 elif (( $(echo "$temp >= $max_temp" |bc -l) )); then
+    getip
     curl -H "Content-Type: application/json" -d '{"username": "Raspberry Pi", "content": "'"J'ai trop chaud ! \nMa température est de : $temp °C. \nJe me mets et sécurité et arrêté tous mes systèmes !"'"}' $webhooks_url
 
 elif (( $(echo "$temp >= $warn_temp" |bc -l) )); then
+    getip
     curl -H "Content-Type: application/json" -d '{"username": "Raspberry Pi", "content": "'"J'ai chaud ! \nMa température est de : $temp °C. \nMon ip est : $ip"'"}' $webhooks_url
 else
     echo "RAS"
 fi
+
+function getip {
+    ip="$(curl -s https://api.ipify.org)"
+}
 
 exit 0
